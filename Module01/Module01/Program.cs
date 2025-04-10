c78da3af9ebfc6247f7eebc28f6c2a1514d2616e
VicMalac <victorpdpyt9098@gmail.com>
2025-04-10 11:25:21 -0300

    Initial commit

---
+++ Module01/Module01/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Escreva uma função em C# que receba como parâmetro um inteiro N e retorne um array
+com todos os divisores de N.me}");*/
+
+Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+if  (numero % 2 == 0)
+{
+    Console.WriteLine("Seu numero é divisível por 2");
+}
+else if (numero % 3 == 0)
+{
+    Console.WriteLine("Seu numero é divisível por 3");
+}
+else if (numero % 5 == 0)
+{
+    Console.WriteLine("Seu numero é divisível por 5");
+}
+else if (numero % 7 == 0)
+{
+    Console.WriteLine("Seu numero é divisível por 7");
+}
+else
+{
+    Console.WriteLine("Seu número não divisível por nenhum número primo menor que 10");
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# C-Sharp
+ Todo meu aprendizado na linguagem C# na faculdade de GTI e em cursos avulsos
